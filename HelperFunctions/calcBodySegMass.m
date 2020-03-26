8b3774f560766733b1b8344b8cfafe6ae18e0c5e
mfturra <60787493+mfturra@users.noreply.github.com>
2020-03-26 09:29:22 -0400

    TotalCOMXYZ was modified
    Previously written code was modified to ensure that the locations of the COM for each seg were in the correct place

--- HelperFunctions/calcBodySegMass.m
+++ HelperFunctions/calcBodySegMass.m
@@ -1,66 +1,66 @@
-function [bodySegMass,propWeight] = calcBodySegMass(kgMass)
+function [bodySegMass,segPropWeight] = calcBodySegMass(kgMass)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Calculates totalMass of each body segment based on users overall kgMass
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Torso segMass by totalMass of body
 bodySegMass = [];
-propWeight = [];
+segPropWeight = [];
 
 %Head propWeight
-propWeight.headWeight =             0.0810; %headMass and neckMass 
-bodySegMass.headMass =              propWeight.headWeight*kgMass;
+segPropWeight.headProp =             0.0810; %headMass and neckMass 
+bodySegMass.headMass =              segPropWeight.headProp*kgMass;
 
 %Shoulder propWeight
-propWeight.shoulderWeight =         0.0158;
+segPropWeight.shoulderProp =         0.0158;
 bodySegMass.shoulderMass =          0.0158*kgMass;
 
 %trunkMass calculation
-propWeight.chestWeight =            0.2160; %thoraxMass
-bodySegMass.chestMass =             propWeight.chestWeight*kgMass; 
-propWeight.abdomenWeight =          0.1390;
-bodySegMass.abdomenMass =           propWeight.abdomenWeight*kgMass;
-propWeight.hipWeight =              0.1420; %pelvisMass
-bodySegMass.hipMass =               propWeight.hipWeight*kgMass; 
-propWeight.trunkWeight =            0.4970;
-bodySegMass.trunkMass =             propWeight.trunkWeight*kgMass;
+segPropWeight.chestProp =            0.2160; %thoraxMass
+bodySegMass.chestMass =             segPropWeight.chestProp*kgMass; 
+segPropWeight.abdomenProp =          0.1390;
+bodySegMass.abdomenMass =           segPropWeight.abdomenProp*kgMass;
+segPropWeight.hipProp =              0.1420; %pelvisMass
+bodySegMass.hipMass =               segPropWeight.hipProp*kgMass; 
+segPropWeight.trunkProp =            0.4970;
+bodySegMass.trunkMass =             segPropWeight.trunkProp*kgMass;
 
-assert(propWeight.trunkWeight == propWeight.chestWeight...
-    + propWeight.abdomenWeight + propWeight.hipWeight, 'Torso needs rechecking!')
+assert(segPropWeight.trunkProp == segPropWeight.chestProp...
+    + segPropWeight.abdomenProp + segPropWeight.hipProp, 'Torso needs rechecking!')
 
 %% Upper extremity segMass by total Mass of body
-propWeight.handWeight =             0.0060;
-bodySegMass.handMass =              propWeight.handWeight*kgMass;
-propWeight.forearmWeight =          0.0160;
-bodySegMass.forearmMass =           propWeight.forearmWeight*kgMass;
-propWeight.upperArmWeight =         0.0280;
-bodySegMass.upperArmMass =          propWeight.upperArmWeight*kgMass;
-propWeight.totalArmWeight =         0.0500;
-bodySegMass.totalArmMass =          propWeight.totalArmWeight*kgMass;
+segPropWeight.handProp =             0.0060;
+bodySegMass.handMass =              segPropWeight.handProp*kgMass;
+segPropWeight.forearmProp =          0.0160;
+bodySegMass.forearmMass =           segPropWeight.forearmProp*kgMass;
+segPropWeight.upperArmProp =         0.0280;
+bodySegMass.upperArmMass =          segPropWeight.upperArmProp*kgMass;
+segPropWeight.totalArmProp =         0.0500;
+bodySegMass.totalArmMass =          segPropWeight.totalArmProp*kgMass;
 
-assert(propWeight.totalArmWeight == propWeight.handWeight...
-    + propWeight.forearmWeight + propWeight.upperArmWeight, 'Upper extremity needs rechecking!')
+assert(segPropWeight.totalArmProp == segPropWeight.handProp...
+    + segPropWeight.forearmProp + segPropWeight.upperArmProp, 'Upper extremity needs rechecking!')
 
 %% Lower extremity segMass by total Mass of body
-propWeight.footWeight =             0.0145;
-bodySegMass.footMass =              propWeight.footWeight*kgMass;
-propWeight.legWeight =              0.0465;
-bodySegMass.legMass =               propWeight.legWeight*kgMass;
-propWeight.thighWeight =            0.1000;
-bodySegMass.thighMass =             propWeight.thighWeight*kgMass;
-propWeight.totalLegWeight =         0.1610;
-bodySegMass.totalLegMass =          propWeight.totalLegWeight*kgMass;
+segPropWeight.footProp =             0.0145;
+bodySegMass.footMass =              segPropWeight.footProp*kgMass;
+segPropWeight.legProp =              0.0465;
+bodySegMass.legMass =               segPropWeight.legProp*kgMass;
+segPropWeight.thighProp =            0.1000;
+bodySegMass.thighMass =             segPropWeight.thighProp*kgMass;
+segPropWeight.totalLegProp =         0.1610;
+bodySegMass.totalLegMass =          segPropWeight.totalLegProp*kgMass;
 
-assert(propWeight.totalLegWeight == (propWeight.footWeight...
-    + propWeight.legWeight + propWeight.thighWeight), 'Lower extremity needs rechecking!')
+assert(segPropWeight.totalLegProp == (segPropWeight.footProp...
+    + segPropWeight.legProp + segPropWeight.thighProp), 'Lower extremity needs rechecking!')
 
 % assert((segWeight.totalLegWeight - (segWeight.footWeight...
 %     + segWeight.legWeight + segWeight.thighWeight)) < 1e-10, 'Lower extremity needs rechecking!')
 
 %% Total Body Evaluation by grouping
 
-assert(propWeight.headWeight + propWeight.trunkWeight + (2.*propWeight.totalArmWeight)...
-    + (2.*propWeight.totalLegWeight) == 1, 'Total Body Evaluation needs rechecking!')
+assert(segPropWeight.headProp + segPropWeight.trunkProp + (2.*segPropWeight.totalArmProp)...
+    + (2.*segPropWeight.totalLegProp) == 1, 'Total Body Evaluation needs rechecking!')
 
 end
 
--- HelperFunctions/calcSegWeightCOM.m
+++ HelperFunctions/calcSegWeightCOM.m
@@ -1,66 +1,67 @@
-function [totalCOMXYZ] = calcSegWeightCOM(segCenter,propWeight)
+function [totalCOMXYZ] = calcSegWeightCOM(segCenter,segPropWeight)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %calcCOM individual seg contribution to totalCOM
 %facilitates plotting totalCOMXYZ 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Head Seg
-headCOM =        propWeight.headWeight .* segCenter.headCenter_mar_dim_frame;
+headCOM =        segPropWeight.headProp .* segCenter.headCenter_mar_dim_frame;
 % headCOM =        (bodySegMass.headMass .* segCenter.headCenter_mar_dim_frame);
 
 %% Shoulder Seg
-LShoulderCOM =   propWeight.shoulderWeight .* segCenter.LShoulderCenter_mar_dim_frame;
-RShoulderCOM =   propWeight.shoulderWeight .* segCenter.RShoulderCenter_mar_dim_frame;
+LShoulderCOM =   segPropWeight.shoulderProp .* segCenter.LShoulderCenter_mar_dim_frame;
+RShoulderCOM =   segPropWeight.shoulderProp .* segCenter.RShoulderCenter_mar_dim_frame;
 
 % LShoulderCOM =   bodySegMass.shoulderMass .* segCenter.LShoulderCenter_mar_dim_frame;
 % RShoulderCOM =   bodySegMass.shoulderMass .* segCenter.RShoulderCenter_mar_dim_frame;
 
 %% Chest Seg
-chestCOM =       propWeight.chestWeight .* segCenter.chestCenter_mar_dim_frame;
+chestCOM =       (segPropWeight.chestProp + segPropWeight.abdomenProp)...
+                    .* segCenter.chestCenter_mar_dim_frame;
 %chestCOM =       bodySegMass.chestMass .* segCenter.chestCenter_mar_dim_frame;
 
 %% UpperArm Seg
-LUpperArmCOM =   propWeight.upperArmWeight .* segCenter.LUpperArmCenter_mar_dim_frame;
-RUpperArmCOM =   propWeight.upperArmWeight .* segCenter.RUpperArmCenter_mar_dim_frame;
+LUpperArmCOM =   segPropWeight.upperArmProp .* segCenter.LUpperArmCenter_mar_dim_frame;
+RUpperArmCOM =   segPropWeight.upperArmProp .* segCenter.RUpperArmCenter_mar_dim_frame;
 
 % LUpperArmCOM =   bodySegMass.upperArmMass .* segCenter.LUpperArmCenter_mar_dim_frame;
 % RUpperArmCOM =   bodySegMass.upperArmMass .* segCenter.RUpperArmCenter_mar_dim_frame;
 
 %% Forearm Seg
-LForearmCOM =    propWeight.forearmWeight .* segCenter.LForearmCenter_mar_dim_frame;
-RForearmCOM =    propWeight.forearmWeight .* segCenter.RForearmCenter_mar_dim_frame;
+LForearmCOM =    segPropWeight.forearmProp .* segCenter.LForearmCenter_mar_dim_frame;
+RForearmCOM =    segPropWeight.forearmProp .* segCenter.RForearmCenter_mar_dim_frame;
 
 % LForearmCOM =    bodySegMass.forearmMass .* segCenter.LForearmCenter_mar_dim_frame;
 % RForearmCOM =    bodySegMass.forearmMass .* segCenter.RForearmCenter_mar_dim_frame;
 
 %% Hand Seg
-LHandCOM =       propWeight.handWeight .* segCenter.LHandCenter_mar_dim_frame;
-RHandCOM =       propWeight.handWeight .* segCenter.RHandCenter_mar_dim_frame;
+LHandCOM =       segPropWeight.handProp .* segCenter.LHandCenter_mar_dim_frame;
+RHandCOM =       segPropWeight.handProp .* segCenter.RHandCenter_mar_dim_frame;
 
 % LHandCOM =       bodySegMass.handMass .* segCenter.LHandCenter_mar_dim_frame;
 % RHandCOM =       bodySegMass.handMass .* segCenter.RHandCenter_mar_dim_frame;
 
 %% Hip Seg
-hipCOM =         propWeight.hipWeight .* segCenter.hipCenter_mar_dim_frame;
+hipCOM =         segPropWeight.hipProp .* segCenter.hipCenter_mar_dim_frame;
 
 % hipCOM =          bodySegMass.hipMass .* segCenter.hipCenter_mar_dim_frame;
 
 %% Thigh Seg
-LThighCOM =      propWeight.thighWeight .* segCenter.LThighCenter_mar_dim_frame;
-RThighCOM =      propWeight.thighWeight .* segCenter.RThighCenter_mar_dim_frame;
+LThighCOM =      segPropWeight.thighProp .* segCenter.LThighCenter_mar_dim_frame;
+RThighCOM =      segPropWeight.thighProp .* segCenter.RThighCenter_mar_dim_frame;
 
 % LThighCOM =      bodySegMass.thighMass .* segCenter.LThighCenter_mar_dim_frame;
 % RThighCOM =      bodySegMass.thighMass .* segCenter.RThighCenter_mar_dim_frame;
 
 %% Leg Seg
-LLegCOM =        propWeight.legWeight .* segCenter.LLegCenter_mar_dim_frame;
-RLegCOM =        propWeight.legWeight .* segCenter.RLegCenter_mar_dim_frame;
+LLegCOM =        segPropWeight.legProp .* segCenter.LLegCenter_mar_dim_frame;
+RLegCOM =        segPropWeight.legProp .* segCenter.RLegCenter_mar_dim_frame;
 
 % LLegCOM =        bodySegMass.legMass .* segCenter.LLegCenter_mar_dim_frame;
 % RLegCOM =        bodySegMass.legMass .* segCenter.RLegCenter_mar_dim_frame;
 
 %% Foot Seg
-LFootCOM =       propWeight.footWeight .* segCenter.LFootCenter_mar_dim_frame;
-RFootCOM =       propWeight.footWeight .* segCenter.RFootCenter_mar_dim_frame;
+LFootCOM =       segPropWeight.footProp .* segCenter.LFootCenter_mar_dim_frame;
+RFootCOM =       segPropWeight.footProp .* segCenter.RFootCenter_mar_dim_frame;
 
 % LFootCOM =       bodySegMass.footMass .* segCenter.LFootCenter_mar_dim_frame;
 % RFootCOM =       bodySegMass.footMass .* segCenter.RFootCenter_mar_dim_frame;
