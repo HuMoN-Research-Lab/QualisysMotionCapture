884fdb8cd9dc58c593c771a461792ba965c516c1
mfturra <60787493+mfturra@users.noreply.github.com>
2020-04-24 09:56:24 -0400

    Master script cleaned and modified to calc and plot vel, acc, and jerk for each trial

--- HelperFunctions/calcMar_Vel_Acc_Jerk.m
+++ HelperFunctions/calcMar_Vel_Acc_Jerk.m
@@ -1,12 +1,14 @@
-function [marVel,marAcc,marJerk,marJerk_squared] = calcMar_Vel_Acc_Jerk(totalCOMXYZ)
+function [marVel,marAcc,marJerk,marJerk_squared,LFoot,RFoot] = calcMar_Vel_Acc_Jerk(segCenter,totalCOMXYZ)
 
 %% Marker tracking hip data
 %hipIDs were acquired using the markerLabels
 marVel =  [];
 marAcc =  [];
 marJerk = [];
-% totalCOMXYZ = totalCOMXYZ.';
-%Velocity of hips
+LFoot =   [];
+RFoot =   [];
+
+%% COM vel, acc, and jerk calculations
 marVelx =           diff(totalCOMXYZ(1,:));
 marVelz =           diff(totalCOMXYZ(3,:));
 marVel =            plus(marVelx,marVelz);
@@ -14,8 +16,20 @@ marAcc =            diff(marVel);
 marJerk =           diff(marAcc);
 marJerk_squared =   (marJerk).^2;
 
-%calculates acceleration of hips
-%hipsAcc = [0 0 0; diff(hipsVel)];
+%% RAnkle & LAnkle vel, acc, and jerk calculations
+RFoot_marVelx =           diff(segCenter.RFootCenter_mar_dim_frame(1,:));
+RFoot_marVelz =           diff(segCenter.RFootCenter_mar_dim_frame(3,:));
+RFoot.marVel =            plus(RFoot_marVelx,RFoot_marVelz);
+RFoot.marAcc =            diff(marVel);
+RFoot.marJerk =           diff(marAcc);
+RFoot.marJerk_squared =   (marJerk).^2;
+
+LFoot_marVelx =           diff(segCenter.LFootCenter_mar_dim_frame(1,:));
+LFoot_marVelz =           diff(segCenter.LFootCenter_mar_dim_frame(3,:));
+LFoot.marVel =            plus(LFoot_marVelx,LFoot_marVelz);
+LFoot.marAcc =            diff(marVel);
+LFoot.marJerk =           diff(marAcc);
+LFoot.marJerk_squared =   (marJerk).^2;
 
 end
 
